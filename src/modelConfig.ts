468031d68473add9250b2be1b6e331fb5a6e99fb
Alvaro Samudio <alvarosamudio@protonmail.com>
2023-06-14 09:37:26 -0500

    add more models

--- src/modelConfig.ts
+++ src/modelConfig.ts
@@ -54,6 +54,39 @@ const models: Model[] = [
       id: "llama-7B-full",
     },
   },
+  {
+    config: {
+      FILEPATH: localStorage.getItem("vicuna-7B-full") || null,
+      model: "vicuna-7B-delta-v1.1-full",
+      downloadURL:
+        "https://huggingface.co/lmsys/vicuna-7b-delta-v1.1/resolve/main/pytorch_model-00001-of-00002.bin",
+      requiredRAM: 6,
+      fileSize: 10,
+      id: "vicuna-7B-delta-v1.1-full",
+    },
+  },
+  {
+    config: {
+      FILEPATH: localStorage.getItem("vicuna-7B") || null,
+      model: "vicuna-7B-delta-v1.1",
+      downloadURL:
+        "https://huggingface.co/lmsys/vicuna-7b-delta-v1.1/resolve/main/pytorch_model-00002-of-00002.bin",
+      requiredRAM: 6,
+      fileSize: 4,
+      id: "vicuna-7B-delta-v1.1",
+    },
+  },
+  {
+    config: {
+      FILEPATH: localStorage.getItem("falcon-7B") || null,
+      model: "falcon-7B",
+      downloadURL:
+        "https://huggingface.co/tiiuae/falcon-7b/resolve/main/pytorch_model-00001-of-00002.bin",
+      requiredRAM: 6,
+      fileSize: 4,
+      id: "falcon-7B",
+    },
+  },
 ];
 
 export default models;
